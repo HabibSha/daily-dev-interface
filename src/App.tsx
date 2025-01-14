5fef74e1ba06b6338a026b44bd381506bded6c68
habibsha <habibshahalahi7424@gmail.com>
2025-01-14 10:54:52 +0600

    Set up new project configuration with shadcn ui

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1>hi shadcn</h1>
+      <Button>I am from ShadCn</Button>
+    </>
+  );
+}
+
+export default App;
