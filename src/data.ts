5e3a9d53fe013303889da23d879022ce7c5d9783
habibsha <habibshahalahi7424@gmail.com>
2025-01-16 19:54:18 +0600

    Create json data for dynamic data

---
+++ src/data.ts
@@ -0,0 +1,61 @@
+export const sidebar = [
+  {
+    id: 101,
+    title: "Title1",
+    subBar: [
+      {
+        id: 10,
+        title: "subTitle1",
+      },
+      {
+        id: 11,
+        title: "subTitle2",
+      },
+      {
+        id: 12,
+        title: "subTitle3",
+      },
+    ],
+  },
+  {
+    id: 102,
+    title: "Title2",
+    subBar: [
+      {
+        id: 10,
+        title: "subTitle1",
+      },
+      {
+        id: 11,
+        title: "subTitle2",
+      },
+      {
+        id: 12,
+        title: "subTitle3",
+      },
+    ],
+  },
+];
+
+export const UpperSocial = [
+  {
+    id: 201,
+    icon: "icon1",
+  },
+  {
+    id: 202,
+    icon: "icon2",
+  },
+  {
+    id: 203,
+    icon: "icon3",
+  },
+  {
+    id: 204,
+    icon: "icon4",
+  },
+  {
+    id: 205,
+    icon: "icon5",
+  },
+];
